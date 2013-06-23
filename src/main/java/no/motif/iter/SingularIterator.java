fde1ff5e78945e5600d4a0819a67b02632096efd
Rune Flobakk <flobakk@gmail.com>
2013-06-23 23:32:36 +0200

    Throwing correct exception from next()

--- src/main/java/no/motif/iter/SingularIterator.java
+++ src/main/java/no/motif/iter/SingularIterator.java
@@ -1,5 +1,7 @@
 package no.motif.iter;
 
+import java.util.NoSuchElementException;
+
 /**
  * Wraps a single object in an iterator. The object is returned at most
  * once from {@link #next()}.
@@ -20,7 +22,7 @@ public final class SingularIterator<V> extends ReadOnlyIterator<V> {
 
     @Override
     public V next() {
-        if (hasBeenReturned) throw new UnsupportedOperationException();
+        if (hasBeenReturned) throw new NoSuchElementException();
         hasBeenReturned = true;
         return value;
     }
