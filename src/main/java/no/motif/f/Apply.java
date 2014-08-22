ef73671bc77731965f8847dd139abf6d4c1b435c
Rune Flobakk <flobakk@gmail.com>
2014-08-23 00:46:46 +0200

    method to convert an Fn to Do

--- src/main/java/no/motif/f/Apply.java
+++ src/main/java/no/motif/f/Apply.java
@@ -2,16 +2,20 @@ package no.motif.f;
 
 /**
  * Utilities to adapt how functions are invoked.
- *
- * <p><strong>Note:</strong>
- * This is part of the more advanced API, and might seem
- * a bit intimidating to some. It is however <em>not</em>
- * a requirement to be acquainted with this API and still
- * benefit from using the Motif library. This part of the API
- * should be considered as a small "value-add" for developers who
- * are comfortable with these concepts.</p>
  */
 public final class Apply {
+
+
+    /**
+     * Convert an {@link Fn} to a {@link Do}. This is only usable for <code>Fns</code> which
+     * performs some kind of side effect, and you need to pass it somewhere which expects a
+     * <code>Do</code>.
+     *
+     * @param fn The {@link Fn} to apply as a <code>Do</code>.
+     * @return The resulting {@link Do}
+     */
+    public static final <I> Do<I> justSideEffectOf(final Fn<I, ?> fn) {
+        return new Do<I>() { @Override public void with(I value) { fn.$(value); }}; }
 
 
     /**
@@ -27,11 +31,13 @@ public final class Apply {
 
     /**
      * {@link PartialApplication Partially apply} a {@link Fn2 function taking 2 arguments}.
-     * @see    PartialApplication
+     *
      *
      * @param  fn2 The function to partially apply.
      * @return use {@link PartialApplication#of(Object) of()}
      *         to partially invoke <code>fn2</code> and yield an {@link Fn}
+     *
+     * @see    PartialApplication
      */
     public static final <I1, I2, O> PartialApplication<Fn2<I1, I2, O>, I1, Fn<I2, O>> partially(Fn2<I1, I2, O> fn2) { return new PartialApplication<Fn2<I1, I2, O>, I1, Fn<I2,O>>(fn2) {
         @Override public Fn<I2, O> partiallyApply(final Fn2<I1, I2, O> fn2, final Fn0<I1> v1) { return new Fn<I2, O>() {
@@ -44,10 +50,15 @@ public final class Apply {
      * @param fn The function to partially apply.
      * @return use {@link PartialApplication#of(Object) of()}
      *         to partially invoke <code>fn</code> and yield an {@link Fn0}
+     *
+     * @see    PartialApplication
      */
     public static final <I, O> PartialApplication<Fn<I, O>, I, Fn0<O>> partially(Fn<I, O> fn) { return new PartialApplication<Fn<I, O>, I, Fn0<O>>(fn) {
         @Override public Fn0<O> partiallyApply(final Fn<I, O> fn, final Fn0<I> v1) { return new Fn0<O>() {
             @Override public O $() { return fn.$(v1.$()); }}; }}; }
+
+
+
 
 
     private Apply() {}
---
+++ src/test/java/no/motif/f/ApplyTest.java
@@ -0,0 +1,20 @@
+package no.motif.f;
+
+import static no.motif.Base.cause;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+import org.junit.Test;
+
+public class ApplyTest {
+
+    @Test
+    public void convertFnToDo() {
+        Exception exception = mock(Exception.class);
+        Apply.justSideEffectOf(cause).with(exception);
+        verify(exception, times(1)).getCause();
+        verifyNoMoreInteractions(exception);
+    }
+}
