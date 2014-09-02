dc1665c36d434b2bce6017f5bdf54a830cf79e84
Rune Flobakk <flobakk@gmail.com>
2014-09-03 00:19:35 +0200

    Resolve enums from name, with fallback value. Closes #58

--- src/main/java/no/motif/Enums.java
+++ src/main/java/no/motif/Enums.java
@@ -1,6 +1,9 @@
 package no.motif;
 
+import static java.util.EnumSet.allOf;
 import static no.motif.Base.first;
+import static no.motif.Base.is;
+import static no.motif.Iterate.on;
 import no.motif.f.Fn;
 
 public final class Enums {
@@ -17,8 +20,7 @@ public final class Enums {
      *
      * @param enumType The enum type to resolve constants from.
      */
-    public static <E extends Enum<E>> Fn<String, E> to(final Class<E> enumType) { return new Fn<String, E>() {
-        @Override public E $(String e) { return Enum.valueOf(enumType, e); }}; }
+    public static <E extends Enum<E>> FromName<E> to(Class<E> enumType) { return new FromName<>(enumType); }
 
 
     /**
@@ -28,6 +30,32 @@ public final class Enums {
      */
     public static <E extends Enum<E>> Fn<Enum<?>, E> toSameName(Class<E> enumType) {
         return first(name).then(to(enumType));
+    }
+
+
+    /**
+     * {@link Fn} which resolves enum constants from names (<code>String</code>).
+     * Use {@link #or(Enum)} to yield a fallback value for non-existing constants,
+     * instead of throwing an exception.
+     */
+    public static final class FromName<E extends Enum<E>> implements Fn<String, E> {
+
+        private final Class<E> enumType;
+
+        private FromName(Class<E> enumType) {
+            this.enumType = enumType;
+        }
+
+        @Override
+        public E $(String name) {
+            return Enum.valueOf(enumType, name);
+        }
+
+        public final Fn<String, E> or(final E fallback) {
+            return new Fn<String, E>() { @Override public E $(String e) {
+                return on(allOf(enumType)).map(name).filter(is(e)).map(to(enumType)).head().orElse(fallback);
+            }};
+        }
     }
 
 
--- src/test/java/no/motif/EnumsTest.java
+++ src/test/java/no/motif/EnumsTest.java
@@ -1,19 +1,45 @@
 package no.motif;
 
+import static no.motif.Enums.name;
+import static no.motif.Enums.to;
 import static no.motif.Enums.toSameName;
+import static no.motif.Iterate.on;
 import static no.motif.Singular.the;
 import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.isIn;
+import static org.hamcrest.Matchers.not;
 import static org.junit.Assert.assertThat;
+import static org.junit.Assume.assumeThat;
 
 import org.junit.Test;
+import org.junit.contrib.theories.Theories;
+import org.junit.contrib.theories.Theory;
+import org.junit.runner.RunWith;
 
+import com.pholser.junit.quickcheck.ForAll;
+
+@RunWith(Theories.class)
 public class EnumsTest {
 
-    enum A { X }
+    enum A { X, A, B, C, D, E, F, DEFAULT }
     enum B { X }
 
     @Test
     public void mapBetweenUnrelatedEnumsByName() {
         assertThat(the(A.X).map(toSameName(B.class)), contains(B.X));
+    }
+
+
+    @Theory
+    public void resolveGivenNameAsEnum(@ForAll A a) {
+        assumeThat(a, not(A.DEFAULT));
+        assertThat(the(a.name()).map(to(A.class).or(A.DEFAULT)), contains(a));
+    }
+
+
+    @Theory
+    public void resolveToFallbackValue(@ForAll String unknownName) {
+        assumeThat(unknownName, not(isIn(on(A.values()).map(name).collect())));
+        assertThat(the(unknownName).map(to(A.class).or(A.DEFAULT)), contains(A.DEFAULT));
     }
 }
